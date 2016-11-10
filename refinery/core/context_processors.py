66f4a4d25cbb89d35415dee1fb3a1d36c8d8b42d
Scott Ouellette <scottx611x@gmail.com>
2016-11-10 14:56:02 -0500

    Expose tornado server port

--- refinery/core/context_processors.py
+++ refinery/core/context_processors.py
@@ -47,5 +47,6 @@ def extra_context(context):
         "REFINERY_EXTERNAL_AUTH_MESSAGE":
             settings.REFINERY_EXTERNAL_AUTH_MESSAGE,
         "REFINERY_TUTORIAL_STEPS": settings.REFINERY_TUTORIAL_STEPS,
-        "SOLR_SYNONYM_SEARCH": settings.SOLR_SYNONYMS
+        "SOLR_SYNONYM_SEARCH": settings.SOLR_SYNONYMS,
+        "TORNADO_SERVER_PORT": settings.TORNADO_SERVER_PORT
     }
