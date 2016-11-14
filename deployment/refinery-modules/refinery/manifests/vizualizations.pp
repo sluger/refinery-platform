7a289f4457b47f68f5218f3dec3cba588e80e4fc
Scott Ouellette <scottx611x@gmail.com>
2016-11-14 13:04:13 -0500

    Cleanup `vizualizations.pp`

--- deployment/refinery-modules/refinery/manifests/vizualizations.pp
+++ deployment/refinery-modules/refinery/manifests/vizualizations.pp
@@ -13,6 +13,7 @@ class refinery::vizualizations {
     # Ensure directory to serve higlass static files from exists
     file { "${django_root}/static/source/js/higlass":
       ensure => 'directory',
+      require => Vcsrepo["${project_root}/higlass"],
     }
 
     # Move higlass files to proper directory
@@ -26,6 +27,7 @@ class refinery::vizualizations {
     }
 
   }
+  include higlass
 
   class higlass_server {
 
@@ -37,28 +39,32 @@ class refinery::vizualizations {
       revision => 'dev',
     }
 
-    # Create higlass-server virtualenv and install requirements
+    # Create higlass-server virtualenv
     python::virtualenv { $virtualenv_higlass_server:
       ensure  => present,
       owner   => $app_user,
       group   => $app_group,
-      require => [ Class['venvdeps']],
+      require => Vcsrepo["${project_root}/higlass-server"],
     }
+    # Install higlass-server requirements
     python::requirements { "${project_root}/higlass-server/api/requirements.txt" :
-      virtualenv => "$virtualenv_higlass_server",
+      virtualenv => $virtualenv_higlass_server,
       owner   => $app_user,
       group   => $app_group,
+      require => Python::Virtualenv[$virtualenv_higlass_server]
     }
 
     # Run tornado as a background process
     # (Including this in supervisor causes conflicts as there is now
     # competition for two DJANGO_SETTINGS_MODULEs)
     exec{ "run_tornado_server":
-      command => "sudo python run_tornado.py ${tornado_server_port} > tornado.out 2>&1 &",
+      command => "sudo ${virtualenv_higlass_server}/bin/python run_tornado.py ${tornado_server_port} > tornado.out 2>&1 &",
       user        => $app_user,
       group       => $app_group,
       cwd         => "${project_root}/higlass-server/api/",
-      path => ['/usr/bin/', "${virtualenv_higlass_server}/bin/" ]
+      path => ['/usr/bin/'],
+      require => Python::Requirements["${project_root}/higlass-server/api/requirements.txt"]
     }
   }
+  include higlass_server
 }
